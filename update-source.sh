b9845001d892eaa7f93a7c58bf6e15fbe3cbd9f1
Kazushi (Jam) Marukawa <kaz-marukawa@xr.jp.nec.com>
2019-03-04 09:23:27 +0900

    Correct update-source script again.

--- update-source.sh
+++ update-source.sh
@@ -20,10 +20,11 @@ esac
 
 function update() {
   # 1. Fetch target $BRANCH
-  # 2. Changed local $BRANCH to specify remote $BRANCH
-  #    - This shows errors if local $BRANCH is not ok to fast-forward rebase
-  git fetch origin $OPT && \
-    git fetch origin $BRANCH:$BRANCH $OPT $FOPT
+  git fetch origin $OPT
+
+  # 2. Changed current branch to $BRANCH
+  #    - This shows errors if local $BRANCH has conflicts
+  git checkout $BRANCH
 
   # 3. Change current branch to $BRANCH if current branch is not dirty
   id=`git describe --always --abbrev=0 --match "NOT A TAG" --dirty="-dirty"`
@@ -33,7 +34,6 @@ function update() {
     echo Please commit or stash them.
     exit 1;;
   esac
-  git checkout $BRANCH
   git reset --hard origin/$BRANCH
 }
 
