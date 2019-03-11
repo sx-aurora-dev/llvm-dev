65abef1b11fd1d7ece2a81b2454508fa2e915f28
Kazushi (Jam) Marukawa <kaz-marukawa@xr.jp.nec.com>
2019-03-11 13:13:24 +0900

    Clean build-and-install.sh script

--- build-and-intall.sh
+++ build-and-intall.sh
@@ -9,64 +9,17 @@ fi
 DEST=$(readlink -f $1)
 shift
 
-BUILD_DIR=$(readlink -f ${BUILD_DIR:=build})
+BUILDDIR=$(readlink -f ${BUILDDIR:=build})
 SRCDIR=$(readlink -f ${SRCDIR:=src})
 
 LLVM_DEV_DIR=$(dirname $(readlink -f $0))
 
 #echo LLVM_DEV_DIR=$LLVM_DEV_DIR
 echo BUILD_TYPE=${BUILD_TYPE:=Release}
-echo BUILD_DIR=${BUILD_DIR}
+echo BUILDDIR=${BUILDDIR}
 echo SRCDIR=${SRCDIR}
 JOBS=${JOBS:-j8}
 
-mkdir -p ${BUILD_DIR}
-
-function call_make() {
-        make -f ${LLVM_DEV_DIR}/Makefile \
-            BUILD_TYPE=$BUILD_TYPE DEST=$DEST SRCDIR=$SRCDIR THREAD=$JOBS "$@"
-}
-
-function build_llvm() {
-        cd ${BUILD_DIR}
-        call_make cmake install
-}
-
-function build_ve_csu() {
-        cd ${SRCDIR}
-        call_make ve-csu
-}
-
-function build_compiler_rt() {
-        cd ${BUILD_DIR}
-        call_make compiler-rt
-}
-
-function build_libunwind() {
-        cd ${BUILD_DIR}
-        call_make libunwind
-}
-
-function build_libcxxabi() {
-        cd ${BUILD_DIR}
-        call_make libcxxabi
-}
-
-function build_libcxx() {
-        cd ${BUILD_DIR}
-        call_make libcxx
-}
-
-function build_openmp() {
-        cd ${BUILD_DIR}
-        call_make openmp
-}
-
-(build_llvm)
-(build_ve_csu)
-(build_compiler_rt)
-(build_libunwind)
-(build_libcxxabi)
-(build_libcxx)
-(build_openmp)
+make -f ${LLVM_DEV_DIR}/Makefile BUILD_TYPE=$BUILD_TYPE \
+    DEST=$DEST SRCDIR=$SRCDIR BUILDDIR=$BUILDDIR THREAD=$JOBS all
 
