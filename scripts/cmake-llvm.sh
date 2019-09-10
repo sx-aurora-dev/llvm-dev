63bccb6de27e317b27b9dec7265d768855e9d664
Kazushi (Jam) Marukawa <kaz-marukawa@xr.jp.nec.com>
2019-09-10 17:10:15 +0900

    Add openmp for x86 architecture.

--- scripts/cmake-llvm.sh
+++ scripts/cmake-llvm.sh
@@ -3,6 +3,6 @@
 $CMAKE -G Ninja \
   -DCMAKE_BUILD_TYPE="$BUILD_TYPE" \
   -DLLVM_TARGETS_TO_BUILD="$TARGET" \
-  -DLLVM_ENABLE_PROJECTS="clang;libcxx;libcxxabi;libunwind;compiler-rt" \
+  -DLLVM_ENABLE_PROJECTS="clang;compiler-rt;libcxx;libcxxabi;libunwind;openmp" \
   -DCMAKE_INSTALL_PREFIX=$DEST \
   $SRCDIR/llvm-project/llvm
