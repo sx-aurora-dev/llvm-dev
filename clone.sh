c39a36d0243a13ed27ffacee05a591820b0dd353
Simon Moll <moll@cs.uni-saarland.de>
2019-11-28 09:36:43 +0100

    clone.sh: added branch parameter

--- clone.sh
+++ clone.sh
@@ -1,17 +1,25 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
+LLVM_DEV_DIR=$(dirname $(readlink -f $0))
 REPO=$1
-shift
+BRANCH=$2
+
+if test x$BRANCH = x; then
+        BRANCH=hpce/develop
+	echo "no BRANCH provided.. defaulting to ${BRANCH}"
+fi
 
 if test x$REPO = x; then
         REPO=$(dirname $(cd `dirname $0` && git config remote.origin.url))
+	echo "no repository provided.. defaulting to ${REPO}"
 fi
 
 SRCDIR=$(readlink -f ${SRCDIR:=src})
 
-LLVM_DEV_DIR=$(dirname $(readlink -f $0))
 
+echo BRANCH=${BRANCH}
 echo REPO=${REPO}
 echo SRCDIR=$SRCDIR
+echo LLVM_DEV_DIR=${LLVM_DEV_DIR}
 
-make -f $LLVM_DEV_DIR/Makefile deep REPO=$REPO SRCDIR=$SRCDIR
+make -f ${LLVM_DEV_DIR}/Makefile deep REPO=$REPO SRCDIR=$SRCDIR BRANCH=$BRANCH
