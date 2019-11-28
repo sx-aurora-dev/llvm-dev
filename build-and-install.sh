5962117f2781307c1d8bea6121e89d74b1d9b892
Simon Moll <moll@cs.uni-saarland.de>
2019-11-28 10:14:41 +0100

    hard wired to install to prefix/

--- build-and-install.sh
+++ build-and-install.sh
@@ -2,12 +2,10 @@
 
 set -e
 
-if test x$1 = x; then
-        echo "Usage: $0 DEST_DIR"
-        exit
-fi
-DEST=$(readlink -f $1)
-shift
+# pick installation prefix
+LLVM_DEV_DIR=$(dirname $(readlink -f $0))
+DEST=${LLVM_DEV_DIR}/../prefix
+echo "Installing into ${DEST}"
 
 BUILDDIR=$(readlink -f ${BUILDDIR:=build})
 SRCDIR=$(readlink -f ${SRCDIR:=src})
@@ -18,7 +16,7 @@ LLVM_DEV_DIR=$(dirname $(readlink -f $0))
 echo BUILD_TYPE=${BUILD_TYPE:=Release}
 echo BUILDDIR=${BUILDDIR}
 echo SRCDIR=${SRCDIR}
-JOBS=${JOBS:-j8}
+# JOBS=${JOBS:-j8} # not required for Ninja
 
 make -f ${LLVM_DEV_DIR}/Makefile BUILD_TYPE=$BUILD_TYPE \
     DEST=$DEST SRCDIR=$SRCDIR BUILDDIR=$BUILDDIR THREAD=$JOBS all
