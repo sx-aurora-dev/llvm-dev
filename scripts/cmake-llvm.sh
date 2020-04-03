4d963785d65cff15b679470063b4fdede02147b7
Simon Moll <moll@cs.uni-saarland.de>
2020-04-03 18:10:57 +0200

    build host openmp

--- scripts/cmake-llvm.sh
+++ scripts/cmake-llvm.sh
@@ -3,7 +3,7 @@
 $CMAKE -G Ninja \
   -DCMAKE_BUILD_TYPE="$BUILD_TYPE" \
   -DLLVM_TARGETS_TO_BUILD="$TARGET" \
-  -DLLVM_ENABLE_PROJECTS="clang;libcxx;libcxxabi;libunwind;compiler-rt" \
+  -DLLVM_ENABLE_PROJECTS="clang;libcxx;libcxxabi;libunwind;compiler-rt;openmp" \
   -DCMAKE_EXPORT_COMPILE_COMMANDS=on \
   -DBUILD_SHARED_LIBS=on \
   -DCMAKE_INSTALL_PREFIX=$DEST \
