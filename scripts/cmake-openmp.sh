455930fa9489786d7c2c1c3944c752025576912e
Kazushi (Jam) Marukawa <jam@pobox.com>
2021-11-23 05:57:03 +0900

    [VE] Change cmake-openmp.sh
    Change cmake-openmp.sh to use LLVM_ENABLE_PER_TARGET_RUNTIME_DIR.

--- scripts/cmake-openmp.sh
+++ scripts/cmake-openmp.sh
@@ -1,30 +1,26 @@
 #!/bin/sh
 
 $CMAKE -G Ninja \
+  -DCMAKE_BUILD_TYPE=$BUILD_TYPE \
   -DCMAKE_C_COMPILER=$DEST/bin/clang \
   -DCMAKE_CXX_COMPILER=$DEST/bin/clang++ \
   -DCMAKE_AR=$DEST/bin/llvm-ar \
   -DCMAKE_RANLIB=$DEST/bin/llvm-ranlib \
-  -DCMAKE_C_COMPILER_TARGET="$TARGET" \
-  -DCMAKE_CXX_COMPILER_TARGET="$TARGET" \
-  -DCMAKE_BUILD_TYPE="$BUILD_TYPE" \
-  -DCMAKE_INSTALL_PREFIX="$RESDIR" \
-  -DOPENMP_LIBDIR_SUFFIX="$LIBSUFFIX" \
-  -DCMAKE_CXX_FLAGS="" \
+  -DCMAKE_C_COMPILER_TARGET=$TARGET \
+  -DCMAKE_CXX_COMPILER_TARGET=$TARGET \
+  -DCMAKE_INSTALL_PREFIX=$DEST \
   -DCMAKE_CXX_FLAGS_RELEASE="$OPTFLAGS" \
-  -DCMAKE_C_FLAGS="" \
   -DCMAKE_C_FLAGS_RELEASE="$OPTFLAGS" \
-  -DLIBOMP_ARCH="$OMPARCH" \
-  -DOPENMP_LLVM_TOOLS_DIR=$TOOLDIR \
   -DLLVM_DIR="$DEST/lib/cmake/llvm" \
-  -DZLIB_LIBRARY="/lib/x86_64-linux-gnu/" \
-  -DCMAKE_SKIP_RPATH=true \
+  -DOPENMP_LIBDIR_SUFFIX="/$TARGET" \
+  -DOPENMP_LLVM_TOOLS_DIR=$TOOLDIR \
   -DOPENMP_ENABLE_LIBOMPTARGET_PROFILING=OFF \
+  -DLIBOMP_HAVE_SHM_OPEN_WITH_LRT=ON \
   $SRCDIR/openmp
 
 # Modify lit.site.cfg to test on VE
-sed -e 's:test_openmp_flags = ":test_openmp_flags = "-target ve-linux -frtlib-add-rpath -ldl -lrt :' \
-    -i runtime/test/lit.site.cfg
+#sed -e 's:test_openmp_flags = ":test_openmp_flags = "-target ve-linux -frtlib-add-rpath -ldl -lrt :' \
+#    -i runtime/test/lit.site.cfg
 
 # Add -j1 to llvm-lit
 sed -e 's:llvm-lit:llvm-lit -j1:' \
